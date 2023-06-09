a404b36331a8d3de6da0fe0e1562aa87bef10990
Dola Barai <dola43dc@gmail.com>
2023-06-09 23:42:03 +0600

    jwt added

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 app.use(cors());
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkm5by0.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,13 +44,23 @@ async function run() {
     await client.connect();
 
     const instructorsCollection = client.db('jingleDb').collection('instructors')
+    const classesCollection = client.db('jingleDb').collection('classes')
 
     app.get('/instructors', async (req, res) => {
         const result = await instructorsCollection.find().toArray();
         res.send(result)
     })
 
+    app.get('/classes', async (req, res) => {
+        const result = await classesCollection.find().toArray();
+        res.send(result)
+    })
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
